7e936f4d09e21c93c17b676bd112ae8cb5ad1cb9
oscar5912 <3170615341503@ingenieria.usac.edu.gt>
2023-11-07 19:04:40 -0600

    first commit

---
+++ Hospital/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+
+#for admin signup
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+
+#for student related form
+class DoctorUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model=models.Doctor
+        fields=['address','mobile','department','status','profile_pic']
+
+
+
+#for teacher related form
+class PatientUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class PatientForm(forms.ModelForm):
+  
+    #este es el campo adicional para vincular al paciente y su médico asistente
+     #esto mostrará el modelo desplegable del método __str__ médico que se muestra en html, así que anúlelo
+     #to_field_name esto buscará el valor correspondiente user_id presente en el modelo Doctor y lo devolverá
+    assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Name and Department", to_field_name="user_id")
+    class Meta:
+        model=models.Patient
+        fields=['address','mobile','status','symptoms','profile_pic']
+
+
+
+class AppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    patientId=forms.ModelChoiceField(queryset=models.Patient.objects.all().filter(status=True),empty_label="Patient Name and Symptoms", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+class PatientAppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+#for contact us page
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+
+
